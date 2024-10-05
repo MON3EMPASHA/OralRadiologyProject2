223997dcd66b4c63425b7fbdaeaeb72199974362
Abdelmonem Hatem <monemabdelmonem144@gmail.com>
2024-10-05 21:57:28 +0300

    "Replaced userLogin function implementation with a fetch API call"

--- src/Slices/GeneralSlice.js
+++ src/Slices/GeneralSlice.js
@@ -5,7 +5,22 @@ export const serverURL = "https://oralradiologymsa.rf.gd/";
 // export const serverURL = "http://localhost/Projects/OralRadiology/";
 
 export function userLogin(data) {
-  return methods.post(serverURL + "userLogic.php/Login", data);
+  //return methods.post(serverURL + "userLogic.php/Login", data);
+
+  fetch(serverURL + "userLogic.php/Login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 export function changesInUserProfile(data) {
   return methods.post(serverURL + "userLogic.php/ChangeUserProfile", data);
