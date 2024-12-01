a11026dd54019ad91fb8ccec108abde5c2ae36f1
Abdelmonem Hatem <monemabdelmonem144@gmail.com>
2024-12-01 19:59:59 +0200

    ء

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -12,7 +12,6 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 export default function Home() {
   return (
     <div className="HomePage">
-      Mon3em
       <WelcomeSection />
       <VideoSection />
       <OralRadiologyInfoSection />
